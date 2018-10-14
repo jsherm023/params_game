42a1e32dfd1f9ed28d96fa70919855a0064c2f06
“Joe <joseph.sherman@Loop.colum.edu>
2018-10-14 10:12:14 -0500

    Adds controller, urL, and finished APP.

--- app/controllers/api/names_controller.rb
+++ app/controllers/api/names_controller.rb
@@ -1,2 +1,9 @@
 class Api::NamesController < ApplicationController
+  def name_input
+    @name= params[:name]
+    render "your_name.json.jbuilder"
+  if @name.start_with? 'a'
+    @name = "Hey, your name starts with the first letter of the alphabet!"
+  end
+end
 end
